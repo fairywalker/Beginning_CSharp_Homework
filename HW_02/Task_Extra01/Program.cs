47009324d5180e56cfb6f603df4346c951cfe257
fairywalker <tema-1s@yandex.ru>
2022-07-14 14:58:39 +0300

    Add Task_Extra01

---
+++ HW_02/Task_Extra01/Program.cs
@@ -0,0 +1,22 @@
+﻿//Вклад в банке составляет X рублей. Ежегодно он увеличивается на P процентов,
+//после чего дробная часть копеек отбрасывается.
+//Требуется определить: через сколько лет вклад составит не менее Y рублей.
+
+Console.Write("Введите размер вклада: ");
+double X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество процентов: ");
+int P = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите цель: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+while (X < Y)
+{
+    result++;
+    X = X/100*(100+P);
+}
+
+Console.Write("Результат: " + Convert.ToString(result));
