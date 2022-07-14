9d5d908dd606f0e97a15444b4071cbd0ef285caf
fairywalker <tema-1s@yandex.ru>
2022-07-14 15:25:56 +0300

    Add Task_Extra02

---
+++ HW_02/Task_Extra02/Program.cs
@@ -0,0 +1,19 @@
+﻿//В первый день спортсмен пробежал X километров.
+//В каждый последующий день он увеличивал пробег на 15% от предыдущего дня.
+//По данному числу Y требуется определить номер дня, в который пробег спортсмена составил не менее Y километров.
+
+Console.Write("Расстояние в первый день: ");
+double X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Целевое растояние: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+while (X < Y)
+{
+    result++;
+    X = X * 1.15;
+}
+
+Console.Write("Результат: " + Convert.ToString(result));
