bf4842048743bf84d5c75d73462d8f059f40f56c
fairywalker <tema-1s@yandex.ru>
2022-07-21 15:12:55 +0300

    Add Task_Extra03

---
+++ HW_03/Task_Extra03/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задана последовательность натуральных чисел, завершающаяся числом 0.
+//Требуется определить значение второго по величине элемента в этой последовательности,
+//то есть элемента, который будет наибольшим, если из последовательности удалить наибольший элемент.
+
+Console.WriteLine("Введие последовательность чисел. После ввода каждого числа нажимайте Enter.");
+Console.WriteLine("Чтобы завершить ввод последовательности, введите 0 (ноль).");
+
+int NewNumber = 1;
+int[] Array = new int[100];
+int index = 0;
+int FirstMax = 0;
+int SecondMax = 0;
+int ArrayLength = 0;
+
+while (NewNumber != 0)
+{
+    NewNumber = Convert.ToInt32(Console.ReadLine());
+    Array[index] = NewNumber;
+    index++;
+}
+
+ArrayLength =  index;
+index = 0;
+
+while (index < ArrayLength)
+{
+    FirstMax = Math.Max(FirstMax,Array[index]);
+    index++;
+}
+
+index = 0;
+
+while (index < ArrayLength)
+{
+    
+    if (Array[index] != FirstMax) SecondMax = Math.Max(SecondMax,Array[index]);
+    index++;
+        
+}
+
+Console.Write("Значение второго по величине элемента: ");
+Console.WriteLine(SecondMax);
