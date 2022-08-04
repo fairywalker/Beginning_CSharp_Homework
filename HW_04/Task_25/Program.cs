8a0fa9005ac9922beaa51ec4c6c230093d42231b
fairywalker <tema-1s@yandex.ru>
2022-08-04 12:57:29 +0300

    Add Task_25

---
+++ HW_04/Task_25/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Write("Введите число: ");
+double Number = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+double Power = Convert.ToDouble(Console.ReadLine());
+
+
+double Result = Math.Pow(Number, Power);
+
+Console.Write("Ответ: ");
+Console.Write(Result);
