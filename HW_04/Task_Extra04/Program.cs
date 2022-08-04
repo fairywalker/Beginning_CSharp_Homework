5b8a58e72b83eca179c23d05c75210e6c2f13ef1
fairywalker <tema-1s@yandex.ru>
2022-08-04 13:58:40 +0300

    Add Task_Extra04

---
+++ HW_04/Task_Extra04/Program.cs
@@ -0,0 +1,42 @@
+﻿//Элемент последовательности называется локальным максимумом,
+//если он строго больше предыдущего и последующего элемента последовательности.
+//Первый и последний элемент последовательности не являются локальными максимумами.
+//Задана последовательность натуральных чисел, завершающаяся числом 0.
+//Требуется определить наименьшее расстояние между двумя локальными максимумами последовательности натуральных чисел,
+//завершающейся числом 0.
+
+Console.WriteLine("Введие последовательность чисел. После ввода каждого числа нажимайте Enter.");
+Console.WriteLine("Чтобы завершить ввод последовательности, введите 0 (ноль).");
+
+int NewNumber = 1;
+int[] Array = new int[100];
+int index = 0;
+int ArrayLength = 0;
+int MaxIndex = -1;
+int Result = 999;
+
+while (NewNumber != 0)
+{
+    NewNumber = Convert.ToInt32(Console.ReadLine());
+    Array[index] = NewNumber;
+    index++;
+}
+
+ArrayLength =  index;
+
+index = 1;
+
+while (index < ArrayLength-1)
+{
+    if (Array[index] > Array[index-1] && Array[index] > Array[index+1])
+    {
+        if (MaxIndex != -1) Result = Math.Min(Result, index-MaxIndex);
+        MaxIndex = index;
+    }
+    index++;
+}
+
+if (Result == 999) Result = 0;
+
+Console.Write("Наименьшее расстояние: ");
+Console.Write(Result);
