31a15a67a954b5019da6fcfd7e5d2647041f7371
fairywalker <tema-1s@yandex.ru>
2022-09-14 16:18:03 +0300

    Add array processing

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,11 +3,36 @@
 Console.Write("Введите длину массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 
-string[] arrayIn = new string[length];
+string[]? arrayIn = new string[length];
+string[]? arrayTemp = new string[length];
 
 for (int i = 0; i < length; i++)
 {
-    Console.Write("Введите значение элемента " + i+1 + ": ");
+    Console.Write("Введите значение элемента " + (i+1) + ": ");
     arrayIn[i] = Console.ReadLine();  
 }
 
+
+
+int indexOut = 0;
+
+for (int indexIn = 0; indexIn < length; indexIn++)
+{
+    if (arrayIn[indexIn].Length <= 3)
+    {
+        arrayTemp[indexOut] = arrayIn[indexIn];
+        indexOut++;
+    }    
+}
+
+string[]? arrayOut = new string[indexOut];
+
+for (int i = 0; i < indexOut; i++)
+{
+    arrayOut[i] = arrayTemp[i];       
+}
+
+Console.Write("Исходный массив: ");
+Console.WriteLine("[ " + string.Join(", ", arrayIn) + " ]");
+Console.Write("Результат: ");
+Console.WriteLine("[ " + string.Join(", ", arrayOut) + " ]");
