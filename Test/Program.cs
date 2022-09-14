a31aa21cf7a060ae8865e5eaa7af3dac191ec86e
fairywalker <tema-1s@yandex.ru>
2022-09-14 15:36:55 +0300

    Add array filling

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Контрольная работа
 
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayIn = new string[length];
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write("Введите значение элемента " + i+1 + ": ");
+    arrayIn[i] = Console.ReadLine();  
+}
+
